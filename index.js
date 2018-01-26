a34cf33e54c207313602396102d4262bae3f3b99
Adrien Lacquemant <verblodung@gmail.com>
2018-01-26 15:57:19 -0500

    feat: looping through users

--- index.js
+++ index.js
@@ -6,59 +6,63 @@
 
 */
 
-var https = require("https");
-var username = "alaq";
+var https = require('https')
+var usernames = ['alaq', 'alexv']
 
-var options = {
-  host: "api.github.com",
-  path: "/users/" + username + "/events",
-  method: "GET",
-  headers: { "user-agent": "node.js" }
-};
+function checkUser(username) {
+  var options = {
+    host: 'api.github.com',
+    path: '/users/' + username + '/events',
+    method: 'GET',
+    headers: { 'user-agent': 'node.js' }
+  }
 
-var request = https.request(options, function(response) {
-  var body = "";
-  response.on("data", function(chunk) {
-    body += chunk.toString("utf8");
-  });
+  var request = https.request(options, function(response) {
+    var body = ''
+    response.on('data', function(chunk) {
+      body += chunk.toString('utf8')
+    })
 
-  response.on("end", function() {
-    var gitUserInfo = JSON.parse(body)[0];
-    var dateCreated = gitUserInfo.created_at;
-    var parseDateCreated = dateCreated.split("-");
-    var createdAtMonth = parseDateCreated[1];
-    var createdAtDay = parseDateCreated[2].slice(0, 2);
-    var createdAtFinal = parseInt(createdAtMonth + createdAtDay);
+    response.on('end', function() {
+      var gitUserInfo = JSON.parse(body)[0]
+      var dateCreated = gitUserInfo.created_at
+      var parseDateCreated = dateCreated.split('-')
+      var createdAtMonth = parseDateCreated[1]
+      var createdAtDay = parseDateCreated[2].slice(0, 2)
+      var createdAtFinal = parseInt(createdAtMonth + createdAtDay)
 
-    var monthsInYear = {
-      Jan: "1",
-      Feb: "2",
-      Mar: "3",
-      Apr: "4",
-      May: "5",
-      Jun: "6",
-      Jul: "7",
-      Aug: "8",
-      Sep: "9",
-      Oct: "10",
-      Nov: "11",
-      Dec: "12"
-    };
+      var monthsInYear = {
+        Jan: '1',
+        Feb: '2',
+        Mar: '3',
+        Apr: '4',
+        May: '5',
+        Jun: '6',
+        Jul: '7',
+        Aug: '8',
+        Sep: '9',
+        Oct: '10',
+        Nov: '11',
+        Dec: '12'
+      }
 
-    var time = new Date().getTime();
-    var date = new Date(time);
-    var todayDate = date.toString();
-    var todayParse = todayDate.split(" ");
-    var currentDay = todayParse[2];
-    var currentMonthWord = todayParse[1];
-    var currentMonthNum = monthsInYear[currentMonthWord];
-    var currentMonthFinal = currentMonthWord + currentDay;
-    var currentDateFinal = parseInt(currentMonthNum + currentDay);
+      var time = new Date().getTime()
+      var date = new Date(time)
+      var todayDate = date.toString()
+      var todayParse = todayDate.split(' ')
+      var currentDay = todayParse[2]
+      var currentMonthWord = todayParse[1]
+      var currentMonthNum = monthsInYear[currentMonthWord]
+      var currentMonthFinal = currentMonthWord + currentDay
+      var currentDateFinal = parseInt(currentMonthNum + currentDay)
 
-    var latestCommitDateMatchesTodaysDate = currentDateFinal === createdAtFinal;
+      var latestCommitDateMatchesTodaysDate = currentDateFinal === createdAtFinal
 
-    console.log("moment of trrrruubaby: ", latestCommitDateMatchesTodaysDate );
-  });
-});
+      console.log(username, 'commited today', latestCommitDateMatchesTodaysDate)
+    })
+  })
 
-request.end();
+  request.end()
+}
+
+usernames.forEach(user => checkUser(user))
