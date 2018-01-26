fb55f58f21c24d1bb91c36dc2d91a58b66378149
Rohan Saigal <rohan4saigal@gmail.com>
2018-01-25 21:26:16 -0500

    match final commit's date with todays date

--- index.js
+++ index.js
@@ -7,28 +7,61 @@
 */
 
 var https = require("https");
-var username = 'jquery';
+var username = "alaq";
 
 var options = {
-    host: 'api.github.com',
-    path: '/users/' + username + '/events',
-    method: 'GET',
-    headers: {'user-agent': 'node.js'}
+  host: "api.github.com",
+  path: "/users/" + username + "/events",
+  method: "GET",
+  headers: { "user-agent": "node.js" }
 };
 
-var request = https.request(options, function(response){
-var body = '';
-response.on("data", function(chunk){
-    body += chunk.toString('utf8');
-});
+var request = https.request(options, function(response) {
+  var body = "";
+  response.on("data", function(chunk) {
+    body += chunk.toString("utf8");
+  });
 
-response.on("end", function(){
-    var obj = JSON.parse(body)[0];
-    console.log('adrien', obj)
-    var arrKeys = Object.keys(obj);
-    //console.log('!!!!!!!!!!!', arrKeys)
-    //console.log("Body: ", JSON.parse(body)[0]);
-    });
+  response.on("end", function() {
+    var gitUserInfo = JSON.parse(body)[0];
+    var dateCreated = gitUserInfo.created_at;
+    var parseDateCreated = dateCreated.split("-");
+    var createdAtMonth = parseDateCreated[1];
+    var createdAtDay = parseDateCreated[2].slice(0, 2);
+    var createdAtFinal = parseInt(createdAtMonth + createdAtDay);
+
+
+    var monthsInYear = {
+      Jan: "1",
+      Feb: "2",
+      Mar: "3",
+      Apr: "4",
+      May: "5",
+      Jun: "6",
+      Jul: "7",
+      Aug: "8",
+      Sep: "9",
+      Oct: "10",
+      Nov: "11",
+      Dec: "12"
+    };
+
+    var time = new Date().getTime();
+    var date = new Date(time);
+    var todayDate = date.toString();
+    var todayParse = todayDate.split(" ");
+    var currentDay = todayParse[2];
+    var currentMonthWord = todayParse[1];
+    var currentMonthNum = monthsInYear[currentMonthWord];
+    var currentMonthFinal = currentMonthWord + currentDay;
+    var currentDateFinal = parseInt(currentMonthNum + currentDay);
+
+    var latestCommitDateMatchesTodaysDate = currentDateFinal === createdAtFinal;
+
+
+
+    console.log("moment of trrrruubaby: ", latestCommitDateMatchesTodaysDate );
+  });
 });
 
 request.end();
